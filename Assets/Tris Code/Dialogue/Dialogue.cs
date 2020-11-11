f8560b6447f22c6e592e6e7c19bd55e2e39340a7
TFFavard <tffavard@gmail.com>
2020-11-10 21:00:53 -0800

    Added Dialogue Code

---
+++ Assets/Tris Code/Dialogue/Dialogue.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Dialogue 
+{
+    [TextArea(3, 10)]
+    //string for names and sentences
+    public string[] sentences;
+    public string[] names;
+
+    //different expression sprites
+    public Sprite[] sprites;
+
+
+}

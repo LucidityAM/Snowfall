814a27e83830fbce15b6d3b487b84bb1a7297bf9
LucidityAM <LucidityAM@gmail.com>
2020-11-10 21:27:21 -0800

    Shader Shit -Isaiah

---
+++ Assets/Isaiah/Scripts/ScaryTransition.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+
+public class ScaryTransition : MonoBehaviour
+{
+    public Material chromaticShader;
+    void Start()
+    {
+
+    }
+}
+    
+

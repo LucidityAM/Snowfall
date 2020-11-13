5f132641d1a25a7ec68725e85a29a1c875221717
LucidityAM <LucidityAM@gmail.com>
2020-11-12 18:43:53 -0800

    Transition script - Isaiah

--- Assets/Isaiah/Scripts/ScaryTransition.cs
+++ Assets/Isaiah/Scripts/ScaryTransition.cs
@@ -8,7 +8,13 @@ public class ScaryTransition : MonoBehaviour
     public Animator anim;
     void Start()
     {
+        
+    }
 
+    IEnumerator StartAnimation()
+    {
+        while (anim.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f)
+            yield return null;
     }
 }
     

976712543f49b428908d9c0188a29a033cf7d0ef
LucidityAM <LucidityAM@gmail.com>
2020-11-14 21:50:04 -0800

    Shader shit - Isiaah

--- Assets/Isaiah/Scripts/ScaryTransition.cs
+++ Assets/Isaiah/Scripts/ScaryTransition.cs
@@ -1,22 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.Rendering.PostProcessing;
 
 public class ScaryTransition : MonoBehaviour
 {
-    public Animator anim;
+    public Animator chromatic;
+    public Animator distortion;
+
+
+    public GameObject chromaticGO;
+    public GameObject distortionGO;
+    public GameObject staticGO;
+
+    public RenderPipelineAsset URP;
+    public RenderPipelineAsset twoD;
+
     void Start()
     {
-        
+        StartCoroutine(StartAnimation());
     }
 
     IEnumerator StartAnimation()
     {
-        while (anim.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f)
+        GraphicsSettings.renderPipelineAsset = twoD;
+
+        while (chromatic.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f)
             yield return null;
 
-        anim.SetTrigger("Distortion");
+        GraphicsSettings.renderPipelineAsset = URP;
+
+        distortionGO.SetActive(true);
+
+        distortion.SetTrigger("Distortion");
+
+        yield return new WaitForSeconds(3f);
+
+        staticGO.SetActive(true);
+        chromaticGO.GetComponent<SpriteRenderer>().enabled = false;
+
+        yield return new WaitForSeconds(3f);
+
+        distortionGO.SetActive(false);
+        staticGO.SetActive(false);
+        chromaticGO.GetComponent<SpriteRenderer>().enabled = true;
+
+        GraphicsSettings.renderPipelineAsset = twoD;
+
     }
 }
     

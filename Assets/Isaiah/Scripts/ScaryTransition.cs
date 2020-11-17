e11ec64af656bd325fe510d8c055356e13fadee1
LucidityAM <LucidityAM@gmail.com>
2020-11-16 23:52:23 -0800

    TRANSITION IS DONE BITCHES - ISAIAH

--- Assets/Isaiah/Scripts/ScaryTransition.cs
+++ Assets/Isaiah/Scripts/ScaryTransition.cs
@@ -7,47 +7,71 @@ using UnityEngine.Rendering.PostProcessing;
 public class ScaryTransition : MonoBehaviour
 {
     public Animator chromatic;
-    public Animator distortion;
-
 
     public GameObject chromaticGO;
-    public GameObject distortionGO;
     public GameObject staticGO;
+    public GameObject DissolveGO;
 
-    public RenderPipelineAsset URP;
-    public RenderPipelineAsset twoD;
+    public Material dissolveMat;
+
+    float fade = 1f;
+
+    bool isDissolving;
 
     void Start()
     {
+        fade = 1f;
         StartCoroutine(StartAnimation());
     }
 
     IEnumerator StartAnimation()
     {
-        GraphicsSettings.renderPipelineAsset = twoD;
 
         while (chromatic.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f)
             yield return null;
 
-        GraphicsSettings.renderPipelineAsset = URP;
-
-        distortionGO.SetActive(true);
-
-        distortion.SetTrigger("Distortion");
-
-        yield return new WaitForSeconds(3f);
-
         staticGO.SetActive(true);
+
+        yield return new WaitForSeconds(.1f);
+
         chromaticGO.GetComponent<SpriteRenderer>().enabled = false;
 
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(4.5f);
 
-        distortionGO.SetActive(false);
+        DissolveGO.SetActive(true);
+
+        yield return new WaitForSeconds(.2f);
+
         staticGO.SetActive(false);
-        chromaticGO.GetComponent<SpriteRenderer>().enabled = true;
+        chromaticGO.GetComponent<SpriteRenderer>().enabled = false;
 
-        GraphicsSettings.renderPipelineAsset = twoD;
+        yield return new WaitForSeconds(2f);
+        
+        Dissolve();
+       
+    }
 
+    public void Dissolve()
+    {
+        isDissolving = true;
+    }
+
+    void Update()
+    {
+
+        if (isDissolving)
+        {
+            fade -= Time.deltaTime *.4f;
+
+            if (fade <= 0f)
+            {
+                fade = 0f;
+                isDissolving = false;
+                DissolveGO.SetActive(false);
+            }
+
+            dissolveMat.SetFloat("_Fade", fade); 
+        }
     }
 }
     

05d1eaf13dff1d17cdc42ae35b5ee5bee9b9d2a1
SeraphCelestial <trayden@drkmind.com>
2020-12-07 19:35:45 -0800

    Reconstructed/Improved the Snowball Throwing Code

---
+++ Assets/Trayden Code/Scripts/KeyItemActionList.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyItemActionList : MonoBehaviour
+{
+    IEnumerator KeyItem1Action()
+    {
+        Debug.Log("Action 1 Completed");
+        yield return null;
+    }
+
+    IEnumerator KeyItem2Action()
+    {
+        Debug.Log("Action 2 Completed");
+        yield return null;
+    }
+}
---
+++ Assets/Trayden Code/Scripts/KeyItemActionTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyItemActionTrigger : MonoBehaviour
+{
+    public string gameobjectName;
+    public GameObject ActionController;
+    public GameObject keyItem;
+    private float speed = 1.5f;
+    private Vector2 velocity = Vector2.zero;
+    void Start()
+    {
+        ActionController = GameObject.FindGameObjectWithTag("Action Controller");
+        keyItem = ActionController.GetComponent<SnowballThrow>().keyItem;
+    }
+
+    void Update()
+    {
+        gameObject.transform.position = Vector2.SmoothDamp(gameObject.transform.position, keyItem.transform.position, ref velocity, .15f, Mathf.Infinity, Time.deltaTime);
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(!collision.gameObject.CompareTag("Snowball"))
+        {
+            gameobjectName = collision.gameObject.name.ToString();
+            ActionController.GetComponent<KeyItemActionList>().StartCoroutine(gameobjectName + "Action");
+            Destroy(this.gameObject);
+        }
+    }
+}

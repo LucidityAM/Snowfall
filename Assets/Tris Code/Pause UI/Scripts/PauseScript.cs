82d00f464a1688744aaf0a5116754f14d8a24fc1
TFFavard <tffavard@gmail.com>
2020-12-07 19:50:17 -0800

    Pause Animation

--- Assets/Tris Code/Pause UI/Scripts/PauseScript.cs
+++ Assets/Tris Code/Pause UI/Scripts/PauseScript.cs
@@ -5,25 +5,28 @@ using UnityEngine;
 public class PauseScript : MonoBehaviour
 {
     public GameObject pauseBG;
-    public GameObject blur;
+    //public GameObject blur;
 
     private Animator pauseBGAnim;
-    private Animator blurAnim;
+    //private Animator blurAnim;
 
 
     #region Things that need to be turned off
     public PlayerMovement playerMovement;
     #endregion
 
+    private bool paused;
 
     void Awake()
     {
+        paused = false;
+
         pauseBG.SetActive(false);
-        blur.SetActive(false);
+        //blur.SetActive(false);
 
         #region Animators
         pauseBGAnim = pauseBG.GetComponent<Animator>();
-        blurAnim = blur.GetComponent<Animator>();
+        //blurAnim = blur.GetComponent<Animator>();
         #endregion
     }
     // Start is called before the first frame update
@@ -34,29 +37,35 @@ public class PauseScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) && paused == false)
         {
-
+            StartCoroutine("OpenMenu");
+        }
+        else if(Input.GetKeyDown(KeyCode.Escape) && paused == true)
+        {
+            StartCoroutine("CloseMenu");
         }
     }
 
     public IEnumerator OpenMenu()
     {
+        paused = true;
         ToggleScripts();
-        blur.SetActive(true);
-        blurAnim.SetTrigger("IsOpen");
-        yield return new WaitForSeconds(0.4f);
+        //blur.SetActive(true);
+        //blurAnim.SetTrigger("IsOpen");
+        yield return new WaitForSeconds(0.1f);
         pauseBG.SetActive(true);
-        pauseBGAnim.SetTrigger("IsOpen");
+        pauseBGAnim.SetBool("IsOpen", true);
     }
 
     public IEnumerator CloseMenu()
     {
+        paused = false;
         ToggleScripts();
-        pauseBGAnim.SetTrigger("IsOpen");
+        pauseBGAnim.SetBool("IsOpen", false);
         yield return new WaitForSeconds(0.4f);
         pauseBG.SetActive(false);
-        blurAnim.SetTrigger("IsOpen");
+        //blurAnim.SetTrigger("IsOpen");
         yield return new WaitForSeconds(0.3f);
     }
 

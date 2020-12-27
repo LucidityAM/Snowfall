b8c217d97cbae269aed031c46d0a3bb6cc9f18b9
TFFavard <tffavard@gmail.com>
2020-12-26 18:49:28 -0800

    Finished pause programming and implented alpha version of npc

---
+++ Assets/Tris Code/Pause UI/Scripts/UIConditions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIConditions : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Fake")
+        {
+            PauseConditions.fakeside = true;
+        } else if (collision.gameObject.tag == "Real")
+        {
+            PauseConditions.fakeside = false;
+        }
+
+    }
+}

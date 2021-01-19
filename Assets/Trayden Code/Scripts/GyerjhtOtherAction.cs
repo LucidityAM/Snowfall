3e2915f5b9a4286fa1b2c4c36905aa4e18c6b2b4
SeraphCelestial <trayden@drkmind.com>
2021-01-18 19:08:24 -0800

    Rebalanced Gyerjht Speed

--- Assets/Trayden Code/Scripts/GyerjhtOtherAction.cs
+++ Assets/Trayden Code/Scripts/GyerjhtOtherAction.cs
@@ -18,11 +18,18 @@ public class GyerjhtOtherAction : MonoBehaviour
         finalPos = new Vector3(player.transform.position.x, gameObject.transform.position.y);
         if(player.transform.position.x >= 135)
         {
+            ActivateGyerjht();
            hasPassedThreshold = true;
         }
         if(hasPassedThreshold == true)
         {
             gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, finalPos, .20f);
         }
+    }
+
+    IEnumerator ActivateGyerjht()
+    {
+        //Monster Noise goes here
+        yield return new WaitForSeconds(7.0f);
     }
 }

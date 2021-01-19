d33d9f6552b2383c7447199b4d78afd52aec63e6
SeraphCelestial <trayden@drkmind.com>
2021-01-18 18:35:55 -0800

    Finished Snowpile Puzzle Logic

---
+++ Assets/Trayden Code/Scripts/GyerjhtOtherAction.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyerjhtOtherAction : MonoBehaviour
+{
+    public Vector3 finalPos;
+    private GameObject player;
+    private bool hasPassedThreshold = false;
+
+    void Start() 
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update() 
+    {
+        finalPos = new Vector3(player.transform.position.x, gameObject.transform.position.y);
+        if(player.transform.position.x >= 135)
+        {
+           hasPassedThreshold = true;
+        }
+        if(hasPassedThreshold == true)
+        {
+            gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, finalPos, .20f);
+        }
+    }
+}
---
+++ Assets/Trayden Code/Scripts/SnowpileOtherAction.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowpileOtherAction : MonoBehaviour
+{
+    public GameObject gyerjht;
+
+    private void OnTriggerEnter2D(Collider2D collision) 
+    {
+        if(collision.gameObject == gyerjht && gameObject.transform.position.y > -1.5f)
+        {
+            gyerjht.SetActive(false);
+            gameObject.SetActive(false);
+        }
+    }
+}
